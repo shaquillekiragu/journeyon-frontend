2df961a2fe0aad3d2f31949cc182218f654534d3
Liza <liza.khmelnitskaya@sainsburys.co.uk>
2025-07-29 15:20:41 +0100

    top page logo

--- src/pages/ProgressPage.tsx
+++ src/pages/ProgressPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 export default function ProgressPage(): React.ReactElement {
   return (
     <div className="min-h-screen bg-gray-50">
+      <Header />
       <Navbar />
       <main className="container mx-auto px-6 py-8">
         <h1 className="text-3xl font-bold text-gray-800">Progress page</h1>

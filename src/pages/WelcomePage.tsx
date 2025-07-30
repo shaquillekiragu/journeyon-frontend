50647ec6e6b87240586ac61107b1ab1794ea3ea3
Shaquille Kiragu <shaquille@toru.digital>
2025-07-30 13:47:06 +0100

    Build welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage(): React.ReactElement {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+      <main className="flex justify-center items-center gap-50 mt-[30vh]">
+        <Link to="/login">
+          <div className="border-2 px-40 py-20 rounded-3xl text-2xl hover:cursor-pointer">
+            Login
+          </div>
+        </Link>
+        <Link to="/signup">
+          <div className="border-2 px-40 py-20 rounded-3xl text-2xl hover:cursor-pointer">
+            Sign up
+          </div>
+        </Link>
+      </main>
+    </>
+  );
 }

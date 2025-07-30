b10bfa92d424e922ea1a4a3b7987808fffb9d97f
Ibrahim Yusuf <ibrahim.yusuf@gap.uk.com>
2025-07-30 11:47:10 +0100

    Get very basic structure of the welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage(): React.ReactElement {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <Navbar />
+      <main className="container text-center w-full mx-auto">
+        <section className="px-6 py-8 h-100 mb-3 text-left text-neutral-50 bg-sky-700">
+          <div className="text-3xl m-auto relative top-50">
+            <h1>
+              Progress with <strong>support.</strong>
+            </h1>
+            <h1>
+              Growing with <strong>confidence.</strong>
+            </h1>
+          </div>
+        </section>
+        <section className="mx-auto columns-2">
+          <Link to={"/login"}>
+            <button>Log In</button>
+          </Link>
+          <br />
+          <Link to={"/signup"}>
+            <button>Sign Up</button>
+          </Link>
+        </section>
+      </main>
+    </div>
+  );
 }

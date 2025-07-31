7f5acbd9c63535ddc4fe98f5ae380f6b899037e4
Olivia Donohoe <olivia.donohoe@sainsburys.co.uk>
2025-07-31 18:55:47 +0100

    cleaner design idea

--- src/components/DiaryHome.tsx
+++ src/components/DiaryHome.tsx
@@ -1,20 +1,7 @@
 import type React from "react";
 
 const DiaryHome: () => React.ReactElement = () => (
-    <div className="bg-orange-100 px-5 rounded-lg p-2">
-        <h2 className="text-center underline text-neutral-950">Diary</h2>
-        <div className="grid grid-cols-2">
-            <div className="text-left">
-                <h3 className="font-bold">Placeholder Entry - 1/1/1970</h3>
-                <p>This is some placeholder text.</p>
-            </div>
-            <div className="text-right">
-                <button className="blueBg text-white px-4 py-2 rounded">
-                    View my diary
-                </button>
-            </div>
-        </div>
-    </div>
+  <button className="blueBg text-white px-4 py-2 rounded">View my diary</button>
 );
 
 export default DiaryHome;

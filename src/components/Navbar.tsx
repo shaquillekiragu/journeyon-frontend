b3ad5334a93b38faed2d5a998a00fbabf923da73
Shaquille Kiragu <shaquille@toru.digital>
2025-08-01 11:56:07 +0100

    Remove temp logged in user tracker

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/UserContext";
+import { useData } from "../contexts/DataContext";
 
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { loggedInUser, setLoggedInUser, isLoggedIn, setIsLoggedIn } =
-    useAuth();
+    useData();
 
   const isActive = (path: string) => {
     return location.pathname === path;
@@ -18,71 +18,63 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <nav className="w-full shadow-md border-b border-gray-200 bg-theme-blue">
-        <div className="w-full px-6 py-4">
-          <div className="flex items-center justify-evenly w-full">
-            {/* JourneyON */}
-            <div className="flex-1 text-center">
-              <Link
-                to="/home"
-                className={`text-lg ${
-                  isActive("/home") ? "font-extrabold" : "font-light"
-                } hover:opacity-80 transition-all duration-200`}
-                style={{ color: "#fdfbf1" }}
-              >
-                Home
-              </Link>
-            </div>
+    <nav className="w-full shadow-md border-b border-gray-200 bg-theme-blue">
+      <div className="w-full px-6 py-4">
+        <div className="flex items-center justify-evenly w-full">
+          {/* JourneyON */}
+          <div className="flex-1 text-center">
+            <Link
+              to="/home"
+              className={`text-lg ${
+                isActive("/home") ? "font-extrabold" : "font-light"
+              } hover:opacity-80 transition-all duration-200`}
+              style={{ color: "#fdfbf1" }}
+            >
+              Home
+            </Link>
+          </div>
 
-            {/* Diary */}
-            <div className="flex-1 text-center">
-              <Link
-                to="/diary"
-                className={`text-lg ${
-                  isActive("/diary") ? "font-extrabold" : "font-light"
-                } hover:opacity-80 transition-all duration-200`}
-                style={{ color: "#fdfbf1" }}
-              >
-                Diary
-              </Link>
-            </div>
+          {/* Diary */}
+          <div className="flex-1 text-center">
+            <Link
+              to="/diary"
+              className={`text-lg ${
+                isActive("/diary") ? "font-extrabold" : "font-light"
+              } hover:opacity-80 transition-all duration-200`}
+              style={{ color: "#fdfbf1" }}
+            >
+              Diary
+            </Link>
+          </div>
 
-            {/* Progress */}
-            <div className="flex-1 text-center">
-              <Link
-                to="/progress"
-                className={`text-lg ${
-                  isActive("/progress") ? "font-extrabold" : "font-light"
-                } hover:opacity-80 transition-all duration-200`}
-                style={{ color: "#fdfbf1" }}
-              >
-                Progress
-              </Link>
-            </div>
+          {/* Progress */}
+          <div className="flex-1 text-center">
+            <Link
+              to="/progress"
+              className={`text-lg ${
+                isActive("/progress") ? "font-extrabold" : "font-light"
+              } hover:opacity-80 transition-all duration-200`}
+              style={{ color: "#fdfbf1" }}
+            >
+              Progress
+            </Link>
+          </div>
 
-            {/* Sign Out */}
-            <div className="flex-1 text-center">
-              <button
-                onClick={handleSignOut}
-                className={`text-lg font-light hover:opacity-80 transition-all duration-200 ${
-                  isLoggedIn ? "!visible" : "!hidden"
-                }`}
-                style={{ color: "#fdfbf1" }}
-              >
-                Sign Out
-              </button>
-            </div>
+          {/* Sign Out */}
+          <div className="flex-1 text-center">
+            <button
+              onClick={handleSignOut}
+              className={`text-lg font-light hover:opacity-80 transition-all duration-200 ${
+                isLoggedIn ? "!visible" : "!hidden"
+              }`}
+              style={{ color: "#fdfbf1" }}
+            >
+              Sign Out
+            </button>
           </div>
         </div>
-      </nav>
-      <p className="text-center font-semibold text-lg mt-3">
-        (Temp feature - for tracking login/logout functionality)
-      </p>
-      <p className="text-center font-semibold text-2xl my-3">
-        Logged in user: {loggedInUser?.user?.email}
-      </p>
-    </>
+      </div>
+    </nav>
   );
 };
 
